2b9b3dad8552c3d614f996603780e4bef668341c
RO35ERT <trebrorobertsbob@gmail.com>
2023-07-16 00:46:35 +0200

    up to date

--- app.js
+++ app.js
@@ -1,11 +1,38 @@
 
-numbers = document.querySelectorAll('#num')
-calscreen = document.querySelector('.screen')
+numbers = document.querySelectorAll('#num');
+calscreen = document.querySelector('.screen');
+
+root = document.querySelector('#root');
+pi = document.querySelector('#pi')
 
 clear = document.querySelector('#clear');
+del = document.querySelector('#del');
+
+operator = document.querySelectorAll('#op');
+
+
+equalto = document.querySelector('#equalto');
 
 clear.addEventListener('click',()=>{
     calscreen.innerText = "";
+})
+
+del.addEventListener('click',()=>{
+   
+    const nums = calscreen.innerText;
+    const newnum = nums.slice(0, nums.length-1);
+    calscreen.innerText = newnum;    
+})
+
+root.addEventListener('click',()=>{
+    const nums = calscreen.innerText;
+    const numbedroot = Math.sqrt(nums);
+
+    calscreen.innerText= numbedroot;
+})
+
+pi.addEventListener('click',()=>{
+    calscreen.innerText += Math.PI; 
 })
 
 numbers.forEach(e => {
@@ -13,3 +40,19 @@ numbers.forEach(e => {
         calscreen.innerText += e.value;
     })
 });
+
+operator.forEach((e)=>{
+    e.addEventListener('click',()=>{
+        calscreen.innerText += e.value;
+    })
+})
+
+equalto.addEventListener('click',()=>{
+    const nums = calscreen.innerText;
+    const answer = eval(nums)
+
+    calscreen.innerText = answer;
+})
+
+
+
